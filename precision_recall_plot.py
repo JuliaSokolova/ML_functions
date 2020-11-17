b6acfb3fec45b096e3af654b16e0900ec65b6bbe
Julia Sokolova <sokolova.js@gmail.com>
2020-11-17 11:26:37 -0800

    work on precision-recall plot

--- precision_recall_plot.py
+++ precision_recall_plot.py
@@ -1,15 +1,21 @@
 from sklearn.metrics import precision_recall_curve
 import matplotlib.pyplot as plt
+import numpy as np
 
 def precision_recall_curve(y, y_predict_proba):
     precisions, recalls, thresholds = precision_recall_curve(y, y_predict_proba)
     fig, ax = plt.subplots()
-    ax.plot(thresholds, precisions[: -1]), "b--", label="Precision")
+    ax.plot(thresholds, precisions[: -1], "b--", label="Precision")
     ax.plot(thresholds, recalls[:-1], "g-", label='Recall')
     ax.set_xticks(thresholds)
-    # add x labels
-    # add y ticks & labeles
+    # ax.set_xticklabel(thresholds)
+    y = np.arange(0, 1.1, 0.1)
+    ax.set_ytics(y)
+    ax.set_yticklabel(y)
     # add grid
+    ax.grid(True)
 
     ax.set_title('Precision and recall versus the decision threshold')
+
+    plt.show()
 
